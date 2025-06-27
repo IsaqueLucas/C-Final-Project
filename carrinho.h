b2dbb962fa1ddc110e177dc7d0d59529d2446d48
IsaqueLucas <75181813+IsaqueLucas@users.noreply.github.com>
2025-06-27 07:18:18 -0300

    Update carrinho.h

--- carrinho.h
+++ carrinho.h
@@ -1,23 +1,10 @@
 #ifndef CARRINHO_H
 #define CARRINHO_H
 
-#include "armas.h"
+#include "usuarios.h"
 
-typedef struct ItemCarrinho {
-    Arma* arma;
-    struct ItemCarrinho* prox;
-} ItemCarrinho;
-
-typedef struct {
-    ItemCarrinho* inicio;
-    ItemCarrinho* fim;
-} FilaCarrinho;
-
-// Operações do carrinho
-void inicializar_carrinho(FilaCarrinho* carrinho);
-void adicionar_ao_carrinho(FilaCarrinho* carrinho, Arma* arma);
-void mostrar_carrinho(FilaCarrinho carrinho);
-void finalizar_compra(FilaCarrinho* carrinho, Usuario* usuario);
-void liberar_carrinho(FilaCarrinho* carrinho);
+void adicionarAoCarrinho(Sessao *sessao, Arma *arma);
+void finalizarCompra(Sessao *sessao);
+void liberarCarrinho(CarrinhoNode *inicio);
 
 #endif
