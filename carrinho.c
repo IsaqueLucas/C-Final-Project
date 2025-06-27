432ed48fa8c5e831c14527a20b80d1ef720cd709
IsaqueLucas <75181813+IsaqueLucas@users.noreply.github.com>
2025-06-27 07:17:58 -0300

    Update carrinho.c

--- carrinho.c
+++ carrinho.c
@@ -1,64 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "carrinho.h"
-#include "usuarios.h"
 
-void inicializar_carrinho(FilaCarrinho* carrinho) {
-    carrinho->inicio = carrinho->fim = NULL;
-}
-
-void adicionar_ao_carrinho(FilaCarrinho* carrinho, Arma* arma) {
-    ItemCarrinho* novo = (ItemCarrinho*) malloc(sizeof(ItemCarrinho));
+void adicionarAoCarrinho(Sessao *sessao, Arma *arma) {
+    CarrinhoNode *novo = (CarrinhoNode *)malloc(sizeof(CarrinhoNode));
     novo->arma = arma;
     novo->prox = NULL;
 
-    if (carrinho->fim == NULL) {
-        carrinho->inicio = carrinho->fim = novo;
+    if (!sessao->carrinhoInicio) {
+        sessao->carrinhoInicio = sessao->carrinhoFim = novo;
     } else {
-        carrinho->fim->prox = novo;
-        carrinho->fim = novo;
+        sessao->carrinhoFim->prox = novo;
+        sessao->carrinhoFim = novo;
     }
-
-    printf("Arma '%s' adicionada ao carrinho.\n", arma->nome);
+    printf("Arma adicionada ao carrinho: %s\n", arma->nome);
 }
 
-void mostrar_carrinho(FilaCarrinho carrinho) {
-    printf("\n--- Carrinho de Compras ---\n");
-    ItemCarrinho* atual = carrinho.inicio;
-    if (atual == NULL) {
+void finalizarCompra(Sessao *sessao) {
+    if (!sessao->carrinhoInicio) {
         printf("Carrinho vazio.\n");
         return;
     }
-    while (atual != NULL) {
-        printf("-> %s | Preço: R$%.2f\n", atual->arma->nome, atual->arma->preco);
+
+    CarrinhoNode *atual = sessao->carrinhoInicio;
+    while (atual) {
+        adicionarAoInventario(sessao->usuario, atual->arma);
+        printf("Comprada: %s\n", atual->arma->nome);
+        CarrinhoNode *remover = atual;
         atual = atual->prox;
+        free(remover);
     }
+
+    sessao->carrinhoInicio = sessao->carrinhoFim = NULL;
+    printf("Compra finalizada com sucesso!\n");
 }
 
-void finalizar_compra(FilaCarrinho* carrinho, Usuario* usuario) {
-    ItemCarrinho* atual = carrinho->inicio;
-    float total = 0;
-
-    while (atual != NULL) {
-        adicionar_arma_inventario(usuario, atual->arma);
-        total += atual->arma->preco;
-
-        ItemCarrinho* temp = atual;
-        atual = atual->prox;
+void liberarCarrinho(CarrinhoNode *inicio) {
+    while (inicio) {
+        CarrinhoNode *temp = inicio;
+        inicio = inicio->prox;
         free(temp);
     }
-
-    carrinho->inicio = carrinho->fim = NULL;
-
-    printf("\nCompra finalizada com sucesso! Total: R$%.2f\n", total);
-}
-
-void liberar_carrinho(FilaCarrinho* carrinho) {
-    ItemCarrinho* atual = carrinho->inicio;
-    while (atual != NULL) {
-        ItemCarrinho* temp = atual;
-        atual = atual->prox;
-        free(temp);
-    }
-    carrinho->inicio = carrinho->fim = NULL;
 }
